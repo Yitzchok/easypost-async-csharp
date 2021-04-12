4014fc254aae6811bacb7d5ef7d0d5bd064eabe4
Yitzchok <78982+Yitzchok@users.noreply.github.com>
2021-04-12 00:54:41 -0400

    Return Task for the tests and check for more properties should be set correctly

--- EasyPostTest/ReportTest.cs
+++ EasyPostTest/ReportTest.cs
@@ -6,6 +6,9 @@
  * All Rights Reserved
  */
 
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using EasyPost;
 
@@ -22,28 +25,36 @@ namespace EasyPostTest
             _client = new EasyPostClient("cueqNZUb3ldeWTNX7MU3Mel8UXtaAMUi");
         }
 
-        [TestMethod]
-        public void TestCreateAndRetrieve()
+        [TestMethod, Ignore("Manual Test. See comment in create report request")]
+        public async Task TestCreateAndRetrieve()
         {
-            var report = _client.CreateReport("shipment", new Report {
+            var createReport = new Report
+            {
                 IncludeChildren = true,
                 // Unfortunately, this can only be run once a day. If you need to test more than that change the date here.
-                //EndDate = DateTime.Parse("2016-06-01"),
-            }).Result;
+                //StartDate = DateTime.Parse("2016-06-03"),
+                //EndDate = DateTime.Parse("2016-06-04"),
+            };
+
+            var report = await _client.CreateReport("shipment", createReport);
+
             Assert.IsNotNull(report.Id);
+            Assert.AreEqual(createReport.StartDate, report.StartDate);
+            Assert.AreEqual(createReport.EndDate, report.EndDate);
+            Assert.AreEqual("new", report.Status);
             Assert.IsTrue(report.IncludeChildren);
 
-            var retrieved = _client.GetReport("shipment", report.Id).Result;
+            var retrieved = await _client.GetReport("shipment", report.Id);
             Assert.AreEqual(report.Id, retrieved.Id);
         }
 
         [TestMethod]
-        public void TestList()
+        public async Task TestList()
         {
-            var reportList = _client.ListReports("shipment").Result;
+            var reportList = await _client.ListReports("shipment");
             Assert.AreNotEqual(0, reportList.Reports.Count);
 
-            var nextReportList = reportList.Next(_client).Result;
+            var nextReportList = await reportList.Next(_client);
             Assert.AreNotEqual(reportList.Reports[0].Id, nextReportList.Reports[0].Id);
         }
     }
